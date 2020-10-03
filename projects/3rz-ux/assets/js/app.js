69ad0c9a06eba8f654edeeff9acff8e649e864a3
welisonmenezes <welisonmenezes@hotmail.com>
2020-10-03 19:02:08 -0300

    Implement the dashboard page of 3rz-ux project

--- projects/3rz-ux/assets/js/app.js
+++ projects/3rz-ux/assets/js/app.js
@@ -1,13 +1,8 @@
-var InitParticlesJs = function () {
+var InitParticlesJs = function (configFile) {
     var div = document.createElement("DIV");
     div.setAttribute("id", "particles-js");
     document.body.appendChild(div);
-    particlesJS.load(
-        "particles-js",
-        "assets/vendors/particles.js-master/particlesjs-config.json",
-        function () {
-            console.log("callback - particles.js config loaded");
-        }
-    );
+    particlesJS.load("particles-js", configFile, function () {
+        console.log("callback - particles.js config loaded");
+    });
 };
-InitParticlesJs();
