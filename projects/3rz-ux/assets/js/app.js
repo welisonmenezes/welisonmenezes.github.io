e7d0530d4278f613f814655f921988887bfc69e0
welisonmenezes <welisonmenezes@hotmail.com>
2020-10-04 11:46:00 -0300

    Implement the lateral menu

--- projects/3rz-ux/assets/js/app.js
+++ projects/3rz-ux/assets/js/app.js
@@ -1,3 +1,4 @@
+// start the background particles
 var InitParticlesJs = function (configFile) {
     var div = document.createElement("DIV");
     div.setAttribute("id", "particles-js");
@@ -6,3 +7,13 @@ var InitParticlesJs = function (configFile) {
         console.log("callback - particles.js config loaded");
     });
 };
+
+// open the lateral menu
+var OpenLateralMenu = function() {
+    document.querySelector('body').classList.add('lateral-menu-opened');
+}
+
+// close the lateral menu
+var CloseLateralMenu = function() {
+    document.querySelector('body').classList.remove('lateral-menu-opened');
+}
